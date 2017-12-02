97b03457511ad7adbf3bd9f1098240d2bae74b79
GA-MO <thawatchai.deartk@gmail.com>
2017-12-03 00:26:32 +0700

    remove all

--- configs/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  name: 'react-skrollr',
-  classNamePrefix: 'react-skrollr'
-}
--- src/index.js
+++
@@ -1,2 +0,0 @@
-export { default as ParallaxProvider } from './ParallaxProvider'
-export { default as Parallax } from './Parallax'
